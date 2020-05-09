4bd082016e0a0ffd80b7623aca9ec880064b3864
Rahul Menon <menonrahul02@gmail.com>
2020-05-09 17:37:59 -0500

    Create and parse craft argument

--- craft-parser/main.py
+++ craft-parser/main.py
@@ -1,5 +1,15 @@
+import argparse
+
+
+def setup_arguments():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("craft", help="Path to craft file")
+
+  return parser.parse_args()
+  
 
 def main():
+  args = setup_arguments()
   print('main')
 
 
