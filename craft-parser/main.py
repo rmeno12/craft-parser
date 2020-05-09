acbc9a6be11c9228bee43f959c8841bb139a9c87
Rahul Menon <menonrahul02@gmail.com>
2020-05-09 17:45:57 -0500

    Install pylint

--- craft-parser/main.py
+++ craft-parser/main.py
@@ -1,12 +1,11 @@
 import argparse
 
-
 def setup_arguments():
   parser = argparse.ArgumentParser()
   parser.add_argument("craft", help="Path to craft file")
 
   return parser.parse_args()
-  
+
 
 def main():
   args = setup_arguments()
