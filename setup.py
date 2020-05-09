d95be6b7af28d496bd735e94fbbaef6c94c72cb8
Rahul Menon <menonrahul02@gmail.com>
2020-05-09 16:21:18 -0500

    Setup project structure and setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='craft-parser', 
+      version='0.1',
+      description='A script to analyze parts of a Kerbal Space Program .craft file',
+      author='Rahul Menon',
+      url='https://github.com/rmeno12/craft-parser',
+      license='MIT',
+      zip_safe=False)
