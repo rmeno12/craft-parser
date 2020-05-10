acd40fca6557180230401b86af7979940929c998
Rahul Menon <menonrahul02@gmail.com>
2020-05-10 17:45:54 -0500

    Include resources in mass calculation

--- craft-parser/craft_parser.py
+++ craft-parser/craft_parser.py
@@ -2,15 +2,24 @@ class CraftParser:
   @staticmethod
   def calculate_mass(craft_filepath: str, masses: dict) -> int:
     parts = []
+    craft_lines = []
     with open(craft_filepath) as craft:
-      for line in craft:
-        if "part = " in line:
-          partname = line.split()[2].split('_')[0]
-          parts.append(partname)
+      craft_lines = [line.strip() for line in craft.readlines()]
+    
+    for index, line in enumerate(craft_lines):
+      if "part = " in line:
+        # Replacing period with underscore because in craft files some parts 
+        # are stored with periods even though their names in cfg files 
+        # use underscores
+        partname = line.split()[2].split('_')[0].replace('.', '_')
+        parts.append((partname, 1))
+      elif 'RESOURCE' in line:
+        resourcename = craft_lines[index + 2].split()[2]
+        amount = float(craft_lines[index + 3].split()[2])
+        parts.append((resourcename, amount))
     
     mass = 0
     for part in parts:
-      print(masses[part])
-      mass += masses[part]
+      mass += masses[part[0]] * part[1]
 
     return mass
